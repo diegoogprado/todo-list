10326e17d6d5644e9107a29f50146afaa91a2bc3
diegoogprado <diegoogprado@hotmail.com>
2024-09-18 20:40:40 -0300

    atualização app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,55 +7,9 @@ import DivTitle from "./components/divTitle"
 import handleTasks from "./hooks/handleTasks"
 
 export default function App() {
-  // const [ tasks, setTasks ] = useState(() => {
-  //   const storedTasks = localStorage.getItem('todo-list')
-  //   if(!storedTasks) return []
-  //   return JSON.parse(storedTasks)
-  // })
+  
+  const { tasks, setTasks,  search, setSearch, filter, setFilter, sort , setSort, addTask, removeTask, completeTask } = handleTasks()
 
-  // const [ search, setSearch ] = useState("")
-
-  // const [ filter, setFilter ] = useState("All")
-
-  // const [ sort , setSort ] = useState("Asc")
-
-  // const addTask = (text, category) => {
-  //   const newTask = {
-  //           id: Math.floor(Math.random() * 1000),
-  //           text,
-  //           category,
-  //           isCompleted: false,
-  //       }
-
-  //   setTasks(state => {
-  //     const newState = [ newTask, ...state]
-  //     localStorage.setItem("todo-list", JSON.stringify(newState))
-  //     return newState
-  //   })
-  // }
-
-
-  // const removeTask = (id) => {
-  //   const newTasks = [...tasks]
-  //   const filteredTasks = newTasks.filter((task) =>
-  //     task.id !== id ? task : null
-  //   )
-  //   setTasks(() => {
-  //     localStorage.setItem("todo-list", JSON.stringify(filteredTasks))
-  //     return filteredTasks
-  //   })
-  // }
-
-  // const completeTask = (id) => {
-  //   const newTasks = [...tasks]
-  //   newTasks.map((task) => task.id === id ? task.isCompleted = !task.isCompleted : task)
-  //   setTasks(() => {
-  //     localStorage.setItem("todo-list", JSON.stringify(newTasks))
-  //      return newTasks
-  //   })
-  // }
-
-   const { tasks, setTasks,  search, setSearch, filter, setFilter, sort , setSort, addTask, removeTask, completeTask } = handleTasks()
   return (
     <div className="app">
       <main>
